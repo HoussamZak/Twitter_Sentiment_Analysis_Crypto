4fe6ee3580469bdb92e1e1b3a57a674a09d655d2
housszak <48485225+HoussamZak@users.noreply.github.com>
2022-05-07 13:23:04 -0500

    done_ready_4_test

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 #config.py
 
-api_key = "ifcMFVPS2uJSKrYuXBvnR7e7Q"
-api_key_secret = "DkzeVZfAZ88wzc0tRyDfgGK4ZegZZZ7FAaAk5P3u6bjWRhgPKk"
+# api_key = "ifcMFVPS2uJSKrYuXBvnR7e7Q"
+# api_key_secret = "DkzeVZfAZ88wzc0tRyDfgGK4ZegZZZ7FAaAk5P3u6bjWRhgPKk"
 
-bearer_token = "AAAAAAAAAAAAAAAAAAAAAPilZAEAAAAAG665O2DB7TxestJ0cqdMokWQGa8%3DgccPbOx4tcC1bDfZEKyzmGerFegy2yJuNLT1yboKATHmPCQbIi"
+# bearer_token = "AAAAAAAAAAAAAAAAAAAAAPilZAEAAAAAG665O2DB7TxestJ0cqdMokWQGa8%3DgccPbOx4tcC1bDfZEKyzmGerFegy2yJuNLT1yboKATHmPCQbIi"
 
-access_token = "1492535312781877253-xnhHyPytBtyAxsbe4zAokPak218CfZ"
-access_token_secret = "S92mRhH2iIrKN34bj1YImNlx0mOygxfrlwOAUI5flQZ3Z"
+# access_token = "1492535312781877253-xnhHyPytBtyAxsbe4zAokPak218CfZ"
+# access_token_secret = "S92mRhH2iIrKN34bj1YImNlx0mOygxfrlwOAUI5flQZ3Z"
+
+api_key = "4084w6OgZgpfCPokKiptoOu0d"
+api_key_secret = "0hoOMwSSWtk1A3MpEz8DnvHldwBeQILWHPAKH3urzqeM1NXbRA"
+
+
+
+bearer_token = "AAAAAAAAAAAAAAAAAAAAACofVQEAAAAA37oDeWUoJjHHjFGcZIB7Hczsxbw%3Dq9faBKvvgAcnkTycCSEg0VnzkloiQJMmGNhBQFRiwYOrwgcaIw"
+
+access_token = "1454477188669923332-emJUzrZpc7De5Oe8VjV4WFc2hcKN33"
+access_toke_secret = "O5kSDeMZMyxTfEcSTSe6ySOSog3tSio4NO3q0Ud3HQxi0"
